45cdf53df37be14a2427fddcb4d9d919069943c4
KhalanderCSE734 <khalandermohammed734@gmail.com>
2025-05-06 21:23:02 +0530

    Added  Appointments Model

---
+++ src/models/appointmentModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+
+const appointmentSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    usn: { type: String, required: true, },//unique: true 
+    semester: { type: String, required: true },
+    department: { type: String, required: true },
+    reason: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+// const appointmentDB = mongoose.connection.useDb("Appointment"); // uses the appointment DB
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+
+
+export default Appointment;
+
+
+
