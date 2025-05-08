fc9b6cc2f93dbcff2862569e28051c9569894a5e
HitishRaoP <hitishraop@gmail.com>
2025-05-08 13:43:19 +0530

    vercel config

--- index.js
+++ index.js
@@ -9,44 +9,46 @@ import { AuthRouter } from "./src/routes/auth.route.js";
 import { CounsellorRouter } from "./src/routes/councellor.route.js";
 import { UserRouter } from "./src/routes/user.route.js";
 
+const app = express();
+
 async function init() {
-	const app = express();
+  app.use(express.json());
 
-	app.use(express.json());
+  app.use(cookieParser());
 
-	app.use(cookieParser());
+  app.use(
+    cors({
+      origin: `${SERVER_CONSTANTS.FRONTEND_URL}`,
+      methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+      credentials: true,
+    })
+  );
 
-	app.use(
-		cors({
-			origin: `${SERVER_CONSTANTS.FRONTEND_URL}`,
-			methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-			credentials: true,
-		}),
-	);
+  app.use(express.urlencoded({ extended: false }));
 
-	app.use(express.urlencoded({ extended: false }));
+  connectDB();
 
-	connectDB();
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Server is Up and Running",
+    });
+  });
 
-	app.get("/", (req, res) => {
-		res.json({
-			message: "Server is Up and Running",
-		});
-	});
+  app.use("/api/user", AuthRouter);
 
-	app.use("/api/user", AuthRouter);
+  app.use("/api/counsellor", CounsellorRouter);
 
-	app.use("/api/counsellor", CounsellorRouter);
+  app.use("/api/appointment", appointmentRouter);
 
-	app.use("/api/appointment", appointmentRouter);
+  app.use("/api/student", UserRouter);
 
-	app.use("/api/student", UserRouter);
-
-	app.listen(SERVER_CONSTANTS.PORT, () => {
-		console.log(
-			`Server is Running on http://localhost:${SERVER_CONSTANTS.PORT}`,
-		);
-	});
+  app.listen(SERVER_CONSTANTS.PORT, () => {
+    console.log(
+      `Server is Running on http://localhost:${SERVER_CONSTANTS.PORT}`
+    );
+  });
 }
 
 init();
+
+export { app };
